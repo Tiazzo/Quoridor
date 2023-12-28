8ef9e23afbc38a61bb87c39133bbacb4b837bad0
Mattia <tech.matto64@gmail.com>
2023-12-28 21:06:06 +0100

    initial commit

---
+++ quoridor/RIT/IRQ_RIT.c
@@ -0,0 +1,135 @@
+/*********************************************************************************************************
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           IRQ_RIT.c
+** Last modified Date:  2014-09-25
+** Last Version:        V1.00
+** Descriptions:        functions to manage T0 and T1 interrupts
+** Correlated files:    RIT.h
+**--------------------------------------------------------------------------------------------------------
+*********************************************************************************************************/
+#include "lpc17xx.h"
+#include "RIT.h"
+#include "../led/led.h"
+#include "../button_EXINT/button.h"
+
+/******************************************************************************
+** Function name:		RIT_IRQHandler
+**
+** Descriptions:		REPETITIVE INTERRUPT TIMER handler
+**
+** parameters:			None
+** Returned value:		None
+**
+******************************************************************************/
+
+extern char led_value;
+
+void RIT_IRQHandler (void)
+{					
+	static int J_select=0;
+	static int J_up = 0;
+	static int J_right = 0;
+	static int J_left = 0;
+	static int J_down = 0;
+		
+	if((LPC_GPIO1->FIOPIN & (1<<29)) == 0){		
+		J_right = 0;
+		J_left = 0;
+		J_down = 0;
+		J_up++;
+		switch(J_up){
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+	else if((LPC_GPIO1->FIOPIN & (1<<28)) == 0){	
+		J_up = 0;
+		J_left = 0;
+		J_down = 0;
+		J_right++;
+		switch(J_right){
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+	else if((LPC_GPIO1->FIOPIN & (1<<27)) == 0){	
+		J_up = 0;
+		J_right = 0;
+		J_down = 0;
+		J_left++;
+		switch(J_left){
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+	else if((LPC_GPIO1->FIOPIN & (1<<26)) == 0){	
+		/* Joytick J_Select pressed p1.25*/
+		/* Joytick J_Down pressed p1.26 --> using J_DOWN due to emulator issues*/
+		J_up = 0;
+		J_right = 0;
+		J_left = 0;
+		J_down++;
+		switch(J_down){
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+	else{
+		J_up = 0;
+		J_right	= 0;
+		J_left = 0;
+		J_down=0;
+	}
+	
+	/* button management */
+	
+	if(down_1!=0){ 
+		if((LPC_GPIO2->FIOPIN & (1<<11)) == 0){	/* KEY1 pressed */
+			down_1++;				
+			switch(down_1){
+				case 2:				/* pay attention here: please see slides to understand value 2 */
+					break;
+				default:
+					break;
+			}
+		}
+		else {	/* button released */
+			down_1=0;			
+			NVIC_EnableIRQ(EINT1_IRQn);							 /* enable Button interrupts			*/
+			LPC_PINCON->PINSEL4    |= (1 << 22);     /* External interrupt 0 pin selection */
+		}
+	}
+	
+	if(down_2!=0){ 
+		if((LPC_GPIO2->FIOPIN & (1<<12)) == 0){	/* KEY1 pressed */
+			down_2++;				
+			switch(down_2){
+				case 2:				/* pay attention here: please see slides to understand value 2 */
+					break;
+				default:
+					break;
+			}
+		}
+		else {	/* button released */
+			down_2=0;			
+			NVIC_EnableIRQ(EINT2_IRQn);							 /* enable Button interrupts			*/
+			LPC_PINCON->PINSEL4    |= (1 << 24);     /* External interrupt 0 pin selection */
+		}
+	}
+	
+  LPC_RIT->RICTRL |= 0x1;	/* clear interrupt flag */
+	
+  return;
+}
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
---
+++ quoridor/button_EXINT/button.h
@@ -0,0 +1,7 @@
+void BUTTON_init(void);
+
+void EINT1_IRQHandler(void);
+void EINT2_IRQHandler(void);
+void EINT3_IRQHandler(void);
+extern int down_1;
+extern int down_2;
