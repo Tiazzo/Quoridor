c0a70cf3316ff21a7adf20e55957530a191c5e3b
Mattia <tech.matto64@gmail.com>
2023-12-29 02:01:11 +0100

    call to matrix draw function

---
+++ quoridor/game.d
@@ -0,0 +1,14 @@
+.\game.o: game.c
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\LPC17xx.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h
+.\game.o: C:\Keil_v5\ARM\ARMCLANG5\Bin\..\include\stdint.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
+.\game.o: C:\Users\mattiacarlino\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
+.\game.o: GLCD/GLCD.h
+.\game.o: timer/timer.h
+.\game.o: RIT/RIT.h
+.\game.o: button_EXINT/button.h
+.\game.o: joystick/joystick.h
